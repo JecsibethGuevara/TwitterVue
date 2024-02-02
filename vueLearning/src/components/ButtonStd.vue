6f8ee93438169d395ebce02f0b13b155d8f8d41c
hi4i <hi4i@directagroup.com>
2024-02-02 16:27:40 -0400

    small start

---
+++ vueLearning/src/components/ButtonStd.vue
@@ -0,0 +1,55 @@
+<template>
+  <button :class="btnClass" @click="handleClick">
+    {{ buttonText }}
+  </button>
+</template>
+
+<script>
+export default {
+  props: {
+    buttonText: {
+      type: String,
+      required: true,
+    },
+    color: {
+      type: String,
+      default: "primary",
+    },
+    onClick: {
+      type: Function,
+      default: () => {},
+    },
+  },
+  computed: {
+    btnClass() {
+      return `btn-${this.color}`;
+    },
+  },
+  methods: {
+    handleClick() {
+      this.onClick();
+    },
+  },
+};
+</script>
+
+<style scoped>
+/* Add your button styling here */
+button {
+  padding: 10px 20px;
+  border: none;
+  cursor: pointer;
+}
+
+.btn-primary {
+  background-color: #007bff;
+  color: #fff;
+}
+
+.btn-secondary {
+  background-color: #6c757d;
+  color: #fff;
+}
+
+/* Add more styles for other colors as needed */
+</style>
---
+++ vueLearning/src/pages/IndexPage.vue
@@ -0,0 +1,13 @@
+<template>
+  <ButtonStd buttonText="hi"> </ButtonStd>
+</template>
+
+<script>
+import ButtonStd from "src/components/ButtonStd.vue";
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "IndexPage",
+  components: { ButtonStd },
+});
+</script>
